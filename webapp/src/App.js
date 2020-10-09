b6440b6341dcca77fb8def6657a975f5feba39ca
Trung Phan <phuoctrung.ppt@gmail.com>
2020-10-09 14:53:41 +0700

    update app, depedencies

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './components/login/Login';
 import RouterURL from './RouterURL/RouterURL';
 
 export function App() {
     return (
-        <RouterURL></RouterURL>
+        // <RouterURL></RouterURL>
+        <Login/>
     );
 }
